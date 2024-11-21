7b62a22089ec1246ea260326179e105c6fbe03ee
tiaturbiina <gabriella.sivla@gmail.com>
2024-11-21 12:12:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'Programação gabizinha',
+    'alem do volei oque a gabi mais gosta de fazer?',
+    'sair com os amigos'
+)
+
+criaCartao(
+    'Programação gabizinha',
+    'qual a marca de roupa que a gabi mais gosta?',
+    'nike'
+)
+
+criaCartao(
+    'Programação gabizinha',
+    'qual o apelido mais top da gabi?',
+    'tia turbina'
+)
+
+criaCartao(
+    'Programação gabizinha',
+    'qual jogadora de volei favorita da gabi?',
+    'Mara Maravilha'
+)
+
+criaCartao(
+    'Programação gabizinha',
+    'qual o carro dos sonhos da gabi?',
+    'fusca'
+)
+
+criaCartao(
+    'Programação gabizinha',
+    'a gabi moraria fora do brasil?',
+    'sim'
+)
+
+criaCartao(
+    'Programação gabizinha',
+    'sabor favorito de clube social da gabi?',
+    'pizza'
+)
+
